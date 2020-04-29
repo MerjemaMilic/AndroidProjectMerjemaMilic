cf30805dc658979f6b69456ccf8d4f533f9db8c4
MerjemaMilic <merjema.milic@gmail.com>
2020-04-29 22:22:07 +0200

    Added transition to login activity

--- app/src/main/java/com/example/myapplication2020/MainActivity.java
+++ app/src/main/java/com/example/myapplication2020/MainActivity.java
@@ -1,17 +1,33 @@
 package com.example.myapplication2020;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-
+private Button login_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login_btn = (Button) findViewById(R.id.login_btn);
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             openLoginActivity();
+            }
+        });
+
+
+    }
+    public void openLoginActivity() {
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
     }
 }
